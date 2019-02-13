48c1ed4253a7b3f58180a4f9b889271e7ea89647
Artem Grachov <grachov2195@gmail.com>
2019-02-13 11:50:41 +0200

    Normalizing emails for login and signup

--- routes/auth.js
+++ routes/auth.js
@@ -14,28 +14,31 @@ router.get('/login', authController.getLogin);
 
 router.post(
   '/login',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter an valid email'),
-  body('email')
-    .custom((value, { req }) => {
-      return User.findOne({ email: value})
-        .then(userDoc => {
-          if (userDoc) return userDoc;
-          return Promise.reject('Invalid email or password');
-        })
-        .then(userDoc => {
-          return bcrypt.compare(req.body.password, userDoc.password)
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter an valid email')
+      .normalizeEmail(),
+    body('email')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value})
+          .then(userDoc => {
+            if (userDoc) return userDoc;
+            return Promise.reject('Invalid email or password');
+          })
+          .then(userDoc => {
+            return bcrypt.compare(req.body.password, userDoc.password)
+            .then(match => {
+              if (match) req.user = userDoc;
+              return match;
+            });
+          })
           .then(match => {
-            if (match) req.user = userDoc;
-            return match;
-          });
-        })
-        .then(match => {
-          if (match) return true;
-          return Promise.reject('Invalid email or password');
-        })
-    }),
+            if (match) return true;
+            return Promise.reject('Invalid email or password');
+          })
+      })
+  ],
   authController.postLogin
 );
 
@@ -45,36 +48,40 @@ router.get('/signup', authController.getSignUp);
 
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter an valid email')
-    .custom((value) => {
-      return User.findOne({ email: value })
-        .then(userDoc => {
-          if (userDoc) {
-            return Promise.reject('User already exists')
-          }
-          return true;
-        })
-  }),
-  body(
-    'password',
-    'Please enter a password with at least 5 characters'
-  )
-    .isLength({
-      min: 5
-    }),
-  body(
-    'confirmPassword'
-  )
-    .custom((value, {
-      req
-    }) => {
-      if (value !== req.body.password) {
-        throw new Error('Passwords have to match');
-      }
-      return true;
-    }),
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter an valid email')
+      .normalizeEmail()
+      .custom((value) => {
+        return User.findOne({ email: value })
+          .then(userDoc => {
+            if (userDoc) {
+              return Promise.reject('User already exists')
+            }
+            return true;
+          })
+        
+      }),
+    body(
+      'password',
+      'Please enter a password with at least 5 characters'
+    )
+      .isLength({
+        min: 5
+      }),
+    body(
+      'confirmPassword'
+    )
+      .custom((value, {
+        req
+      }) => {
+        if (value !== req.body.password) {
+          throw new Error('Passwords have to match');
+        }
+        return true;
+      }),
+  ],
   authController.postSignUp
 );
 
